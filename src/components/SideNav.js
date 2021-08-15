1443ea8e259419368d5226738dac336eecc8c105
Lance Dang <lancedang64@gmail.com>
2021-08-15 10:32:31 +1000

    Added Swal for form reset confirmation

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import templateData from '../templateData';
 import { defaultData } from './App';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
 
 function SideNav(props) {
+	const handleReset = action => {
+		MySwal.fire({
+			icon: 'warning',
+			title: 'Are you sure?',
+			html: 'This will replace/clear all fields in the form',
+			showCancelButton: true,
+			confirmButtonText: `Ok, I am sure`,
+		}).then(result => {
+			if (result.isConfirmed) {
+				props.setPersonData(action === 'reset' ? defaultData : templateData);
+			}
+		});
+	};
+
 	return (
 		<div className='SideNav'>
 			<span onClick={() => props.setEditMode(prev => !prev)}>
@@ -11,11 +29,11 @@ function SideNav(props) {
 
 			<span onClick={() => console.log('download PDF')}>Download PDF</span>
 
-			<span onClick={() => props.setPersonData(templateData)}>
+			<span onClick={() => handleReset('fillWithTemplate')}>
 				Fill with template
 			</span>
 
-			<span onClick={() => props.setPersonData(defaultData)}>Reset form</span>
+			<span onClick={() => handleReset('reset')}>Reset form</span>
 		</div>
 	);
 }
